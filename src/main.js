0e48a83739d18ee53eb9175751f253fb61461a3e
qiaoyiting <qiaoyiting>
2018-12-24 14:18:51 +0800

    handle  info

--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 
 import store from '@/store';
+import * as components from '@/components/index';
 
 import App from './App';
 import router from './router/router';
-import * as components from '@/components/index';
 // import copy from 'fast-copy';
 
 Vue.use(ElementUI);
