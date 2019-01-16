c4ccc559e2e7ce93178a891d14ed743db827e616
qiaoyiting <qiaoyiting>
2019-01-16 19:48:14 +0800

    1

--- model/index.js
+++ model/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'ycxjqyt1.',
+  password: 'Ycxjqyt1.',
   database: 'qyt'
 });
 
